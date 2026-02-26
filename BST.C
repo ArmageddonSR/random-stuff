940af1b4e7685702e1ba022d27538d22b7db9516
ArmageddonSR <151889707+ArmageddonSR@users.noreply.github.com>
2026-02-26 06:37:20 +0000

    thing

---
+++ BST.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct Node
+{
+    int key;
+    struct Node *left, *right;
+};
+
+
+int main(int argc, char* argv[])
+{
+    struct Node *root = NULL;
+
+}
+
+
+void InsertNewNode(struct  Node *root, int val)
+{
+
+
+}
